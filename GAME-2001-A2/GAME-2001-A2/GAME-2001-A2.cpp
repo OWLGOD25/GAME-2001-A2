e68ee274e3cc029076ca4280bb0839edd9a25156
Tyron Fajardo <tyronjf44@gmail.com>
2025-11-29 14:11:59 -0500

    adding the project and git ignore

---
+++ GAME-2001-A2/GAME-2001-A2/GAME-2001-A2.cpp
@@ -0,0 +1,10 @@
+// GAME-2001-A2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
